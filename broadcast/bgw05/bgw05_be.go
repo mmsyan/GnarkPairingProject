05709914ca18e03abb46abfbb8dc447155b15182
mmsyan <88571295+mmsyan@users.noreply.github.com>
2026-02-18 15:17:20 +0800

    feat: start develop bgw05's broadcast

--- broadcast/bgw05/bgw05_be.go
+++ broadcast/bgw05/bgw05_be.go
@@ -1,25 +1,56 @@
 package bgw05
 
+import (
+	"errors"
+	"fmt"
+	"github.com/consensys/gnark-crypto/ecc/bn254"
+	"github.com/consensys/gnark-crypto/ecc/bn254/fr"
+	"math/big"
+)
+
 type MasterPublicKey struct {
+	Bound            int
+	G1ExpAlphaPowers []bn254.G1Affine
+	G2ExpAlphaPowers []bn254.G2Affine
+	G1ExpGamma       fr.Element
 }
 
 type MasterSecretKey struct {
+	Gamma fr.Element
 }
 
 type UserIdentity struct {
+	Identity int
 }
 
 type UserSecretKey struct {
+	SecretKey bn254.G2Affine
 }
 
 type Message struct {
+	Message bn254.GT
 }
 
 type Ciphertext struct {
+	C1 bn254.G1Affine
+	C2 bn254.G1Affine
+	C3 bn254.GT
 }
 
 func Setup(n int) (*MasterPublicKey, *MasterSecretKey, error) {
+	if n < 1 {
+		return nil, nil, fmt.Errorf("invalid parameters")
+	}
+	alpha, err := new(fr.Element).SetRandom()
+	if err != nil {
+		return nil, nil, fmt.Errorf("unable to setup master public key")
+	}
+	gamma, err := new(fr.Element).SetRandom()
+	if err != nil {
+		return nil, nil, fmt.Errorf("unable to setup master public key")
+	}
 
+	g1ExpGamma := new(bn254.G1Affine).ScalarMultiplicationBase(gamma.BigInt(new(big.Int)))
 }
 
 func Extract(i *UserIdentity, msk *MasterSecretKey) (*UserSecretKey, error) {
